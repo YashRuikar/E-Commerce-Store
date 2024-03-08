859d08777b9474fd7b8af49452b62b103498a11a
YashRuikar <yashruikar04@gmail.com>
2024-03-08 16:10:02 +0530

    Services & Trusted

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,20 @@
 import React from "react";
 import HeroSection from "./components/HeroSection";
+import Services from "./components/Services";
+import Trusted from "./components/Trusted";
 
 const Home = () => {
-
   const data = {
-    name: "Yash Store"
-  }
+    name: "Yash Store",
+  };
 
-  return <HeroSection myData={data} />;
+  return (
+    <>
+      <HeroSection myData={data} />
+      <Services />
+      <Trusted />
+    </>
+  );
 };
 
 export default Home;
