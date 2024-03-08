1a4f8074c4f407d30af1bc4a861814e9eb9fdb9a
YashRuikar <yashruikar04@gmail.com>
2024-03-08 16:38:03 +0530

    Contact Page

--- src/Contact.js
+++ src/Contact.js
@@ -33,7 +33,53 @@ const Contact = () => {
     }
   `;
 
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <h2 className="common-heading">Contact Page</h2>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15273.266539109267!2d74.5594633508169!3d16.86022424303485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc1186a8739d50b%3A0x14b4771ad94e71ad!2sSFC%20MEGAA%20MALL!5e0!3m2!1sen!2sin!4v1709894604691!5m2!1sen!2sin"
+        width="100%"
+        height="300"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+
+      <div className="container">
+        <div className="contact-form">
+          <form action="https://formspree.io/f/xayrbobl" method="POST" className="contact-inputs">
+            <input
+              type="text"
+              placeholder="username"
+              name="uesrname"
+              required
+              autoComplete="off"
+            />
+
+            <input
+              type="email"
+              placeholder="Email"
+              name="Email"
+              required
+              autoComplete="off"
+            />
+
+            <textarea
+              name="Message"
+              cols="30"
+              rows="10"
+              required
+              autoComplete="off"
+              placeholder="Enter your message"
+            ></textarea>
+
+            <input type="submit" value="send" />
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Contact;
